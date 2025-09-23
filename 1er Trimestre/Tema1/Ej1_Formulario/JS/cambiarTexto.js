467298980285ecc05a749deaa4bc822d0624eb1e
IanGz200 <iangraziano18@gmail.com>
2025-09-23 13:37:51 +0200

    Ejercicio js

---
+++ 1er Trimestre/Tema1/Ej1_Formulario/JS/cambiarTexto.js
@@ -0,0 +1,29 @@
+function cambiar_texto(){
+    if (document.getElementById("prueba").innerHTML == "Otro texto") {
+
+        document.getElementById("prueba").innerHTML = "Texto original";
+        
+    }else document.getElementById("prueba").innerHTML = "Otro texto"
+    
+    
+}
+
+function cambiar_tamaño(){
+
+    if (document.getElementById("prueba").style.fontSize == "16px") {
+        
+        document.getElementById("prueba").style.fontSize = "100px";
+
+    } else document.getElementById("prueba").style.fontSize = "16px"
+
+}
+
+function cambiar_color() {
+
+    if (document.getElementById("prueba").style.color == "black") {
+        
+        document.getElementById("prueba").style.color = "red";
+
+    } else document.getElementById("prueba").style.color = "black"
+    
+}
